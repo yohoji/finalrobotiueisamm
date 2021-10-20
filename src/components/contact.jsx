d33be666c9271000b4535d89fb4625f1360c6c56
MR SADEK <bezefae@gmail.com>
2021-10-20 01:07:37 +0100

    ready version

---
+++ src/components/contact.jsx
@@ -0,0 +1,225 @@
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const initialState = {
+  name: '',
+  email: '',
+  message: '',
+}
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setState((prevState) => ({ ...prevState, [name]: value }))
+  }
+  const clearState = () => setState({ ...initialState })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, message)
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          clearState()
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+  return (
+    <div>
+      <div id='contact'>
+        <div className='container'>
+          <div className='col-md-8'>
+            <div className='row'>
+              <div className='section-title'>
+                <h2>Join US</h2>
+                <p>
+                  Please fill out the form below to send us ur application and we will
+                  get back to you as soon as possible.
+                </p>
+              </div>
+              <form name='sentMessage' method="POST" validate action="https://docs.google.com/forms/d/e/1FAIpQLScg6FYi-m4BHptoI3d7t8fE8mq0jYkIDnQ7pSaLmvncYgBkfg/formResponse">
+                <div className='row'>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='text'
+                        id='name'
+                        name='entry.1291740113'
+                        className='form-control'
+                        placeholder='First Name'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='text'
+                        id='name'
+                        name='entry.444349995'
+                        className='form-control'
+                        placeholder='Last Name'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='text'
+                        id='name'
+                        name='entry.1241671488'
+                        className='form-control'
+                        placeholder='Level'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='text'
+                        id='name'
+                        name='entry.1210087097'
+                        className='form-control'
+                        placeholder='Section'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='text'
+                        id='name'
+                        name='entry.933118808'
+                        className='form-control'
+                        placeholder='Phone Number'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='email'
+                        id='email'
+                        name='entry.319692905'
+                        className='form-control'
+                        placeholder='Email'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                </div>
+                <div className='form-group'>
+                  <label htmlFor="department">Which department would you like to take part in ?</label>
+                  <select
+                    id="department"
+                    name="entry.308229793"
+                    className='form-control'
+                    required
+                  >
+                    <option value="Project">Project</option>
+                    <option value="Training">Training</option>
+                    <option value="Communication">Communication</option>
+                    <option value="Sponsoring">Sponsoring</option>
+                    <option value="Design">Design</option>
+                  </select>
+                </div>
+                <div className='form-group'>
+                  <input type="hidden" name="entry.308229793_sentinel" />
+                  <p className='help-block text-danger'></p>
+                </div>
+                <div id='success'></div>
+                <button type='submit' className='btn btn-custom btn-lg'>
+                  Send Application
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className='col-md-3 col-md-offset-1 contact-info'>
+            <div className='contact-item'>
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className='fa fa-map-marker'></i> Address
+                </span>
+                {props.data ? props.data.address : 'loading'}
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-phone'></i> Phone
+                </span>{' '}
+                {props.data ? props.data.phone : 'loading'}
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-envelope-o'></i> Email
+                </span>{' '}
+                {props.data ? props.data.email : 'loading'}
+              </p>
+            </div>
+          </div>
+          <div className='col-md-12'>
+            <div className='row'>
+              <div className='social'>
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.facebook : '/'}>
+                      <i className='fa fa-facebook'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.twitter : '/'}>
+                      <i className='fa fa-twitter'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.youtube : '/'}>
+                      <i className='fa fa-youtube'></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
+            <a href='http://www.templatewire.com' rel='nofollow'>
+              TemplateWire
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
